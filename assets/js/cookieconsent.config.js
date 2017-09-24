63a4c6c34b58dee788527be6d692a02e3150982f
Robin Janssens <robin@robinjanssens.net>
2017-09-24 18:53:17 +0200

    added cookie consent and links in footer

---
+++ assets/js/cookieconsent.config.js
@@ -0,0 +1,14 @@
+window.addEventListener("load", function(){
+window.cookieconsent.initialise({
+  "palette": {
+    "popup": {
+      "background": "#383b75"
+    },
+    "button": {
+      "background": "#f1d600"
+    }
+  },
+  "content": {
+    "href": "privacy.html"
+  }
+})});
