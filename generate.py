32e515bf65d6a68ad23dad0f8bd477327ce1e0b2
Robin Janssens <robin@robinjanssens.net>
2017-10-20 16:06:55 +0200

    generate static pages

--- generate.py
+++ generate.py
@@ -1,11 +1,22 @@
+import json
 import codecs
 import markdown
 
-inputFileName = "content/wishlist.md"
-outputFileName = "public/wishlist.html"
+# Read JSON file
+with open('./content.json') as data_file:
+    data = json.load(data_file)
 
-inputFile = codecs.open(inputFileName, mode="r", encoding="utf-8")
-text = inputFile.read()
-html = markdown.markdown(text)
-outputFile = codecs.open(outputFileName, "w", encoding="utf-8", errors="xmlcharrefreplace")
-outputFile.write(html)
+# Generate html pages
+for page in data['pages']:
+    inputFile = codecs.open(page['src'], mode='r', encoding='utf-8')
+    content = inputFile.read()
+    if page['type'] == 'markdown':
+        html = markdown.markdown(content)
+    elif page['type'] == 'html':
+        html = content
+    else:
+        print('unknown type ' + page['title'])
+        continue   # skip this page
+    outputFilePath = 'public/' + page['src'].split('/')[-1].split('.')[0] + '.html'   # 'public/' + last element of source pad + remove extension + add '.html'
+    outputFile = codecs.open(outputFilePath, 'w', encoding='utf-8', errors='xmlcharrefreplace')
+    outputFile.write(html)
